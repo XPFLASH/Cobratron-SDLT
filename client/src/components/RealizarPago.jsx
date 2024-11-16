5efda572df15f1a880535e4c629eda44bb6c0dc6
RICARDO ESCOBAR CESEÑA <95449905+XPFLASH@users.noreply.github.com>
2024-11-15 20:38:40 -0800

    Alert

--- client/src/components/RealizarPago.jsx
+++ client/src/components/RealizarPago.jsx
@@ -6,7 +6,7 @@ const PagoItem = ({ pago, onPagoRealizado }) => {
 
   const handlePago = async () => {
     if (!montoPagado || montoPagado < pago.monto_pago) {
-      alert('Por favor ingresa un monto válido');
+      alert('Por favor ingresa un monto válido (Ejem. 100.00)');
       return;
     }
 
